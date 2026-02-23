6c8e025c2e4f8dbc4ab60b0401a2190230fafdb6
karnika199 <nkarnika199@gmail.com>
2026-02-23 13:39:12 +0530

    Add Jenkins greeting to main method

--- f1.java
+++ f1.java
@@ -1,7 +1,8 @@
 class f1
 {
-public static void main(String[] args)
-{
-System.out.println("Hello World");
-}
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+    System.out.println("Hello Jenkins");
+  }
 }
