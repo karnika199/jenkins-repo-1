d91ccec24d2463b05e00624d4c9652c4f2c4f7dd
karnika199 <nkarnika199@gmail.com>
2026-02-23 13:40:29 +0530

    Update Jenkins greeting message in f1.java

--- f1.java
+++ f1.java
@@ -3,6 +3,6 @@ class f1
   public static void main(String[] args)
   {
     System.out.println("Hello World");
-    System.out.println("Hello Jenkins");
+    System.out.println("Hello from Jenkins");
   }
 }
